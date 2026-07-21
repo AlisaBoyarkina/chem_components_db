1e24c28772c3ae13f70c0d990aea60291222e7b8
AlisaBoyarkina <alicaboarkina14@gmail.com>
2026-07-21 22:56:05 +0300

    Remove accidentally added Desktop folder

--- Desktop/00 универ/Предметы из университета/Проекты из университета/chem_components_db/components/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Component, ComponentAlias, Property,DataSource, ImportLog
-
-
-admin.site.register(Component)
-admin.site.register(ComponentAlias)
-admin.site.register(Property)
-admin.site.register(DataSource)
-admin.site.register(ImportLog)
